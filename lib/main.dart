23ae645eda243914830ff0b1fd27a1949ca2c1d5
Guilherme <guilhermengel1@gmail.com>
2025-10-24 19:23:04 -0300

    Adiconando novas chaves depois de ter vazado as antigas. Rsrs

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gerador_de_senha/firebase_options.dart';
 import 'package:gerador_de_senha/routes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
