92605ab0b9836f258fadb1b2653975e1aa2bd569
Guilherme <guilhermengel1@gmail.com>
2025-10-24 15:46:30 -0300

    Adicionando rotas, screens e lottie

---
+++ lib/screens/LoginERegistro.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginERegistro extends StatelessWidget {
+  const LoginERegistro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("login"),),
+    );
+  }
+}
